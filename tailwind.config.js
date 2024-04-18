5e0cee6425fad43e0985196ec67126f94861ede5
AdlyRadwika <aradwika@gmail.com>
2024-04-18 19:19:35 +0700

    Refactor: change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['ui-sans-serif'],
+    }
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }
